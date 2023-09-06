d62b496e97991940b2ed2cb944750fd9f50f63c9
Kutman <kutmansagymbekuulu@gmail.com>
2023-09-06 20:25:56 +0600

    server change 2

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; // you can use any port number here; i chose to use 3001yar
+const express = require('express');
+const jsonServer = require('json-server');
+const jsonServerAuth = require('json-server-auth');
 
-server.use(middlewares);
-server.use(router);
+const app = express();
+const router = jsonServer.router('db.json'); // Replace 'db.json' with your JSON data file.
 
-server.listen(port);
+// Enable JSON Server Auth Middleware
+app.db = router.db;
+app.use(express.json());
+app.use(jsonServerAuth);
+
+// Define your routes and other middleware as needed.
+// For example:
+app.use('/api', router);
+
+// Start your Express server
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
